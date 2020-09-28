0c77fb43d0d9a1999950874b0b0a10d68a6f6240
Yevhenii Kukhol <yevhenii.kukhol@gmail.com>
2020-09-28 22:09:19 +0300

    block 2

---
+++ lab3/part2/part2/TRTriangle.cs
@@ -0,0 +1,118 @@
+using System;
+
+namespace part2
+{
+    public class TRTriangle : IComparable
+    {
+        protected double a;
+        protected double b;
+        protected double c;
+
+        public TRTriangle()
+        {
+        }
+
+        public TRTriangle(double a, double b, double c)
+        {
+            ValidateTriangleSides(a, b, c);
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double GetPerimeter()
+        {
+            return a + b + c;
+        }
+
+        public double GetArea()
+        {
+            double p = GetPerimeter() / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        private static void ValidateTriangleSides(double a, double b, double c)
+        {
+            if (a < 0 || b < 0 || c < 0)
+            {
+                throw new Exception("Side length must be greater than 0");
+            }
+
+            if (!IsDoubleEquals(Math.Pow(a, 2), Math.Pow(b, 2) + Math.Pow(c, 2)) &&
+                !IsDoubleEquals(Math.Pow(b, 2), Math.Pow(a, 2) + Math.Pow(c, 2)) &&
+                !IsDoubleEquals(Math.Pow(c, 2), Math.Pow(a, 2) + Math.Pow(b, 2))
+            )
+            {
+                throw new Exception("Triangle with that sides cannot exist");
+            }
+        }
+
+        public double GetA()
+        {
+            return a;
+        }
+
+        public double GetB()
+        {
+            return b;
+        }
+
+        public double GetC()
+        {
+            return c;
+        }
+
+        public void SetA(double a)
+        {
+            ValidateTriangleSides(a, b, c);
+            this.a = a;
+        }
+
+        public void SetB(double b)
+        {
+            ValidateTriangleSides(a, b, c);
+            this.b = b;
+        }
+
+        public void SetC(double c)
+        {
+            ValidateTriangleSides(a, b, c);
+            this.c = c;
+        }
+
+        private static bool IsDoubleEquals(double a, double b)
+        {
+            return Math.Abs(a - b) < 0.001;
+        }
+
+        public override string ToString()
+        {
+            return "Triangle : a = " + a + " b = " + b + " c = " + c;
+        }
+
+        public int CompareTo(object obj)
+        {
+            TRTriangle a1 = (TRTriangle) obj;
+            if (this == obj || IsDoubleEquals(a1.GetArea(), GetArea()))
+            {
+                return 0;
+            }
+
+            return a1.GetArea() > GetArea() ? -1 : 1;
+        }
+
+        public static TRTriangle operator *(TRTriangle triangle, double num)
+        {
+            return new TRTriangle(triangle.a * num,
+                triangle.b * num,
+                triangle.c * num);
+        }
+        
+        public static TRTriangle operator *(double num, TRTriangle triangle)
+        {
+            return new TRTriangle(triangle.a * num,
+                triangle.b * num,
+                triangle.c * num);
+        }
+    }
+}
